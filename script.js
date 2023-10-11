fcb5516175f9254b5d66318bb078b39e39bca964
Tristán Marín <tristanmarinlopez@alumnos.tuniversformacion.es>
2023-10-11 10:39:46 +0200

    Añadimos los comentarios

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+ //Añadimos un escuchador que lo que hace es cargar todo el html antes de ejecutar el js para evitar errores
  document.addEventListener("DOMContentLoaded", () => {
   const calcular = document.getElementById("boton");
-
+//Creamos evento para el boton calcular
   calcular.addEventListener("click", () => {
     
     let longitud = document.getElementById("long").value;
     let ancho = document.getElementById("ancho").value;
 
     //Cuando intente meter valores negativos no le dejara
-    
+
     if((longitud >= 0 && longitud <=9) && (ancho >= 0 && ancho <=9) ){
     let area = 0;
     let perimetro = 0;
@@ -15,6 +16,8 @@
         area = parseFloat(longitud) * parseFloat(ancho);
         perimetro = parseFloat(longitud*2)+(ancho*2);
         diagonal=Math.sqrt(Math.pow(longitud,2)+Math.pow(ancho,2))
+
+        //Cambiamos el contenido de parrafos previamente definidos en el html para dar el resultado
         document.getElementById("mensaje").innerHTML = "Area del rectangulo : "+area;
         document.getElementById("mensajePerimetro").innerHTML = "Perimetro del rectangulo: "+ perimetro;
         document.getElementById("mensajeDiagonal").innerHTML = "Diagonal del rectangulo:" + diagonal.toFixed(2);
